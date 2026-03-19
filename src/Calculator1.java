538b82c196b91c99592bdf0746b1c36b8dd0bb05
Minjae <cmj011112@gmail.com>
2026-03-19 12:10:20 +0900

    예외 처리 추가

--- src/Calculator1.java
+++ src/Calculator1.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator1 {
@@ -6,10 +7,17 @@ public class Calculator1 {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            System.out.print("첫 번째 숫자: ");
-            int first_number = sc.nextInt();
-            System.out.print("두 번째 숫자: ");
-            int second_number = sc.nextInt();
+            int first_number = 0;
+            int second_number = 0;
+            try {
+                System.out.print("첫 번째 숫자: ");
+                first_number = sc.nextInt();
+                System.out.print("두 번째 숫자: ");
+                second_number = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("정수를 입력하세요");
+                continue;
+            }
 
             System.out.print("연산자를 입력하세요(+, -, *, /) : ");
             String operator = sc.next();
