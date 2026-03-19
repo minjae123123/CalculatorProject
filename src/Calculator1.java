aec39bfccc55e500f75357daa99b80ccf86dd99a
Minjae <cmj011112@gmail.com>
2026-03-19 12:38:25 +0900

    first_number 부분에 exit 기능 변경

--- src/Calculator1.java
+++ src/Calculator1.java
@@ -7,25 +7,33 @@ public class Calculator1 {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            int first_number = 0;
-            int second_number = 0;
+            int first_number;
+            int second_number;
+
             try {
-                System.out.print("첫 번째 숫자: ");
-                first_number = sc.nextInt();
-                System.out.print("두 번째 숫자: ");
-                second_number = sc.nextInt();
+                System.out.print("첫 번째 숫자 입력 (종료하려면 exit): ");
+                String first_Input = sc.nextLine();
+
+                if (first_Input.equalsIgnoreCase("exit")) {
+                    System.out.println("계산기를 종료합니다!");
+                    break;
+                }
+
+                first_number = Integer.parseInt(first_Input);
+
+                System.out.print("두 번째 숫자 입력: ");
+                String secondInput = sc.nextLine();
+                second_number = Integer.parseInt(secondInput);
+
             } catch (InputMismatchException e) {
-                System.out.println("정수를 입력하세요");
+                System.out.println("정수만 입력하세요.");
                 continue;
             }
+
 
             System.out.print("연산자를 입력하세요(+, -, *, /) : ");
             String operator = sc.next();
 
-            if(operator.equals("exit")) {
-                System.out.println("계산기 종료를 종료합니다!");
-                break;
-            }
 
             switch (operator) {
                 case "+":
