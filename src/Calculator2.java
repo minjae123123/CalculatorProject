43afc21806c8c6813fb02923e4fd7e36176653ee
Minjae <cmj011112@gmail.com>
2026-03-24 17:26:37 +0900

    step2 계산기 과제 커밋

--- src/Calculator2.java
+++ src/Calculator2.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator2 {
@@ -19,35 +18,33 @@ public class Calculator2 {
                 System.out.print("두 번째 숫자 입력: ");
                 second_number = sc.nextInt();
 
+                System.out.print("연산자를 입력하세요(+, -, *, /) : ");
+                String input  = sc.next();
 
-            } catch (NumberFormatException e) {
+                if(input.equals("exit")) {
+                    System.out.println("계산기를 종료합니다!");
+                    break;
+                }
+
+                char operator = input.charAt(0);
+
+                int calculate = calculator.calculate(first_number, second_number, operator);
+                System.out.println(first_number + "" + operator + second_number + "= "+ calculate);
+
+                System.out.println("더 계산하시겠습니까? 더 계산하시려면 아무 키나 눌러주세요(exit 입력 시 종료) : ");
+                String exit = sc.next();
+                if(exit.equals("exit")) {
+                    System.out.println("계산기를 종료합니다.");
+                    break;
+                }
+            } catch (InputMismatchException e) {
                 System.out.println("정수만 입력하세요.");
-                continue;
+                sc.nextLine();
             }
-
-            System.out.print("연산자를 입력하세요(+, -, *, /) : ");
-            String input  = sc.next();
-
-            if(input.equals("exit")) {
-                System.out.println("계산기를 종료합니다!");
-                break;
-            }
-
-            char operator = input.charAt(0);
-
-            int calculate = calculator.calculate(first_number, second_number, operator);
-            System.out.println(first_number + "+" + second_number + "=" + calculate);
-
         }
     }
 
     public static class Calculator {
-
-        private List<Integer> results;
-
-        public Calculator() {
-            this.results = new ArrayList<>();
-        }
 
         private int calculate(int first_number, int second_number,char operator) {
             int result = 0;
@@ -66,16 +63,9 @@ public class Calculator2 {
                 break;
             }
 
-            results.add(result);
+
             return result;
         }
 
-        public List<Integer> getResults() {
-            return results;
-        }
-
-        public void setResults(List<Integer> results) {
-            this.results = results;
-        }
     }
 }
