0e90ec6ad14a36423e251593c6fb85788ea2b907
Minjae <cmj011112@gmail.com>
2026-03-26 15:49:38 +0900

    APP2, APP3 커밋

--- src/ArithmeticCalculator.java
+++ src/ArithmeticCalculator.java
@@ -13,6 +13,24 @@ public class ArithmeticCalculator {
     public int calculate(int first_number,int second_number, char operatorType) {
         OperatorType operatorType1 = OperatorType.fromChar(operatorType);
         int calculation = operatorType1.calculation(first_number, second_number);
+        list.add(calculation);
         return calculation;
+    }
+
+    public List<Integer> getList() {
+        return list;
+    }
+
+    public void setList(List<Integer> list) {
+        this.list = list;
+    }
+
+    public void removeResult(int order) {
+        if(list.isEmpty()) {
+            System.out.println("삭제할 결과가 없습니다.");
+        } else {
+            list.remove(order);
+            System.out.println("선택하신 결과를 삭제하였습니다.");
+        }
     }
 }
