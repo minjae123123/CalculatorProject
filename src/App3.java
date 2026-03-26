0c4afdb3087005fbe947d17c61cec0dfa447cabe
Minjae <cmj011112@gmail.com>
2026-03-26 16:06:30 +0900

    App3 리스트 조회 및 삭제 기능 추가

--- src/App3.java
+++ src/App3.java
@@ -25,8 +25,18 @@ public class App3 {
 
                 int calculate = calculator.calculate(first_number, second_number, operator);
                 System.out.println(first_number  +""+ operator + second_number + "=" + calculate);
+                System.out.println("저장된 결과 목록: " +calculator.getList());
 
-                System.out.println("더 계산하시겠습니까? 더 계산하시려면 아무 키나 눌러주세요(exit 입력 시 종료) : ");
+                System.out.print("리스트 중 하나를 삭제하시겠습니까? (삭제하려면 y 입력) : ");
+                String delete_select = sc.next();
+                if(delete_select.equals("y")) {
+                    System.out.print("리스트에서 삭제하실 인덱스를 입력해주세요: ");
+                    int select_number = sc.nextInt();
+                    calculator.removeResult(select_number);
+                    System.out.println("현재 리스트: " + calculator.getList());
+                }
+
+                System.out.print("더 계산하시겠습니까? 더 계산하시려면 아무 키나 눌러주세요(exit 입력 시 종료) : ");
                 String exit = sc.next();
 
                 if(exit.equals("exit")) {
